5b78b1c0d02a0a2321b6765a3072234794a180df
bitcamp <bitcamp@DESKTOP-CAP80BG>
2021-01-06 18:21:17 +0900

    깃허브

--- src/Test_Git.java
+++ src/Test_Git.java
@@ -8,6 +8,8 @@ public class Test_Git {
 		System.out.println("파일수정ㄴㅁㅇㄴㅁㄴㅁㅇㅁ");
 		System.out.println("지연주");
 		System.out.println("김도윤");
+		System.out.println("Window -> Perspective -> Open Perspective-> other ->git 오픈");
+		System.out.println(" Clone a Git Repository and add 어쩌고~ 클릭-> 깃허브 저장소 url입력->밑에 깃허브 아이디 비밀번호입력");
 	}
 
 }
