10229bf2199f8ec83f51df265f4cd4bb7e304234
suyun <suyun@DESKTOP-7DBPT8A>
2021-01-29 18:33:07 +0900

    kjkkjkj

--- src/PushClass.java
+++ src/PushClass.java
@@ -2,7 +2,8 @@
 public class PushClass {
 
 	public static void main(String[] args) {
-		System.out.println("전송테스트");
+		System.out.println("전송테스트");System.out.println("테스트");System.out.println("전송테스트11111");
+		System.out.println("전송");
 	}
 
 }
--- src/Test_Git2.java
+++ src/Test_Git2.java
@@ -2,10 +2,7 @@
 public class Test_Git2 {
 
 	public static void main(String[] args) {
-		System.out.println("이용섭");
-		System.out.println("ㄴㅁㅇ");
-		System.out.println("김도윤");
-		System.out.println("이클립스 내에서 이렇게 하는 게 맞나요?");
-	}
+	
 
+}
 }
