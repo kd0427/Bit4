26aa118c6aebfdace53d6c89d775b5f873382e04
user <user@DESKTOP-LK3DSGS>
2021-01-29 18:35:31 +0900

    입력lnl

--- src/New_Test.java
+++ src/New_Test.java
@@ -3,6 +3,7 @@ public class New_Test {
 
 	public static void main(String[] args) {
 		System.out.println("파일수정");
+		System.out.println("수정하기");
 	}
 
 }
--- src/PushClass.java
+++ src/PushClass.java
@@ -4,6 +4,7 @@ public class PushClass {
 	public static void main(String[] args) {
 		System.out.println("전송테스트");
 		System.out.println("지연주");
+		System.out.println("왜안돼");
 	}
 
 }
